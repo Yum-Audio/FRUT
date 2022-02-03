2b4b903161e7650274818815b5ce71b50397a9dd
benediktadams <mail@benediktadams.com>
2022-02-03 15:56:33 +0100

    Added build script

---
+++ Build.sh
@@ -0,0 +1,14 @@
+BASE_DIR=$(dirname "$0")
+BUILD_DIR="$BASE_DIR/build"
+
+cd "$BASE_DIR"
+
+if [ ! -d "$BUILD_DIR" ]; then
+mkdir "$BUILD_DIR"
+fi
+
+cd "$BUILD_DIR"
+
+cmake .. -DCMAKE_INSTALL_PREFIX="../prefix" -DJUCE_ROOT="../../JUCE" -DJucer2CMake_JUCE_LICENSE=Indie
+
+cmake --build . --target install --parallel 8
