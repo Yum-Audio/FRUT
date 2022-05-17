8e0e6e9eba09b3dcc724693512a1e17ecd283467
Benedikt Adams <mail@benediktadams.com>
2022-05-17 06:59:07 +0200

    switched -Wno-unused-parameters flag to beginning

--- Build.sh
+++ Build.sh
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 BASE_DIR=$(dirname "$0")
 BUILD_DIR="$BASE_DIR/build"
 
@@ -11,4 +12,20 @@ cd "$BUILD_DIR"
 
 cmake .. -DCMAKE_INSTALL_PREFIX="../prefix" -DJUCE_ROOT="../../JUCE" -DJucer2CMake_JUCE_LICENSE=Indie 
 cmake -E env CXXFLAGS="-Wno-unused-parameter" cmake ..
+=======
+BASE_DIR=$(dirname "$0")
+BUILD_DIR="$BASE_DIR/build"
+
+cd "$BASE_DIR"
+
+if [ ! -d "$BUILD_DIR" ]; then
+mkdir "$BUILD_DIR"
+fi
+
+cd "$BUILD_DIR"
+
+cmake -E env CXXFLAGS="-Wno-unused-parameter" cmake ..
+cmake .. -DCMAKE_INSTALL_PREFIX="../prefix" -DJUCE_ROOT="../../JUCE" -DJucer2CMake_JUCE_LICENSE=Indie 
+
+>>>>>>> 21b0ece7 (switched -Wno-unused-parameters flag to beginning)
 cmake --build . --target install --parallel 8
